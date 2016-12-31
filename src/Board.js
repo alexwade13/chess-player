c422fabe8fccc2c19cbef7ee7c015bc3c69d631d
Alex Wade <alex.wade13@gmail.com>
2016-12-31 16:09:44 -0500

    intial commit

---
+++ src/Board.js
@@ -0,0 +1,41 @@
+import React, { Component, PropTypes } from 'react';
+import Square from './Square';
+import sample from '../sample_board.json';
+
+export default class Board extends Component {
+  static propTypes = {
+  
+  };
+
+  renderSquare(i) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+
+    return (
+      <div key={i}
+           style={{ width: '12.5%', height: '12.5%' }}>
+        <Square x={x}
+                y={y}>
+                
+        </Square>
+      </div>
+    );
+  }
+
+
+  render() {
+    
+    const pieces = sample.game_board.board.pieces.split('/').map((row)=>row.split(""))
+    console.log(pieces)
+    const squares = [];
+    for (let i = 0; i < 64; i++) {
+      squares.push(this.renderSquare(i));
+    }
+
+    return (
+      <div className='Board'>
+        {squares}
+      </div>
+    );
+  }
+}
---
+++ src/Square.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Square extends Component {
+  static propTypes = {
+    x: PropTypes.number,
+    y: PropTypes.number
+    
+  };
+
+  render() {
+
+    const { x, y } = this.props;
+    const black = (x + y) % 2 === 1;
+    const backgroundColor = black ? 'black' : 'white';
+    const color = black ? 'white' : 'black';
+
+    return (
+      <div style={{
+        color,
+        backgroundColor,
+        width: '100%',
+        height: '100%'
+      }}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
